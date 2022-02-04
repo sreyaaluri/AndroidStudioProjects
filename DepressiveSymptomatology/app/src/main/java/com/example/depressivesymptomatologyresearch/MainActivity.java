112929e2909dc0c0d0717b62838dc51e1f529ca7
Sreya Aluri <sreyaaluri@gmail.com>
2022-02-04 17:10:50 -0500

    mood questionnaire logic ready

--- DepressiveSymptomatology/app/src/main/java/com/example/depressivesymptomatologyresearch/MainActivity.java
+++ DepressiveSymptomatology/app/src/main/java/com/example/depressivesymptomatologyresearch/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
                     Intent startIntent = new Intent(getApplicationContext(), sleep_questionnaire.class);
                     startIntent.putExtra(MYNAME, myname);
+                    startIntent.putExtra("SCORE", "0");
 
                     startActivity(startIntent);
                 }
---
+++ DepressiveSymptomatology/app/src/main/java/com/example/depressivesymptomatologyresearch/appetite_questionnaire.java
@@ -0,0 +1,14 @@
+package com.example.depressivesymptomatologyresearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class appetite_questionnaire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appetite_questionnaire);
+    }
+}
