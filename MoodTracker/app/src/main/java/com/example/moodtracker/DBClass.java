fe0346a54cfa894c582f024d11c6292a1d079e3a
DMAIDOH <deborahmaidoh@gmail.com>
2022-03-03 11:01:04 -0500

    updates to DBClass and login/signup pages

--- MoodTracker/app/src/main/java/com/example/moodtracker/DBClass.java
+++ MoodTracker/app/src/main/java/com/example/moodtracker/DBClass.java
@@ -8,6 +8,33 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class DBClass extends SQLiteOpenHelper {
+    //variables for table and column names in loginQuery
+    private String TABLE_ONE_NAME="UserInfo";
+    private static final String UNAME_COL = "username";
+    private static final String PASSWORD_COL = "password";
+    private static final String NAME_COL = "name";
+    private static final String AGE_COL = "age";
+    private static final String GENDER_COL = "gender";
+
+    //variables for table and column names in diaryQuery
+    private String TABLE_TWO_NAME="diary";
+    private static final String DATE_COL = "date";
+    private static final String NOTES_COL = "notes";
+    private static final String EXERCISED_COL = "exercised";
+    private static final String OUTSIDE_COL = "outside";
+    private static final String SOCIALIZED_COL = "socialized";
+    private static final String MEDITATED_COL = "meditated";
+    private static final String READ_COL = "read";
+
+    //variables for table and column names in diaryQuery
+    private String TABLE_THREE_NAME="Tracker";
+    /*private static final String UNAME_COL = "username";
+    private static final String EXERCISED_COL = "exercised";
+    private static final String OUTSIDE_COL = "outside";
+    private static final String SOCIALIZED_COL = "socialized";
+    private static final String MEDITATED_COL = "meditated";
+    private static final String READ_COL = "read"; */
+
 
     // default constructor, factory points to cursor factory->allow cursor, version of db
     public DBClass(Context context, String DATABASE_NAME){
@@ -17,28 +44,32 @@ public class DBClass extends SQLiteOpenHelper {
     // called when db is created for the first time
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // TODO add code to make user info/ login table
-        String loginQuery = "";
+        //user credentials table
+        String loginQuery = "CREATE TABLE" + TABLE_ONE_NAME  + "("
+                + UNAME_COL + " TEXT PRIMARY KEY, "
+                + PASSWORD_COL + " TEXT, "
+                + NAME_COL + " TEXT, "
+                + AGE_COL + " INTEGER, "
+                + GENDER_COL + " TEXT)";
 
-        String diaryQuery = "CREATE TABLE Diary ("
-                + "id  INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + "date TEXT, "
-                + "notes TEXT, "
-                + "exercised INTEGER, " // boolean
-                + "outside INTEGER, " // boolean
-                + "socialized INTEGER, " // boolean
-                + "meditated INTEGER, " // boolean
-                + "read INTEGER)"; // boolean
+        String diaryQuery = "CREATE TABLE" + TABLE_TWO_NAME + "("
+                + UNAME_COL + " TEXT PRIMARY KEY, "
+                + DATE_COL + " TEXT, "
+                + NOTES_COL + " TEXT, "
+                + EXERCISED_COL + " INTEGER, " // boolean
+                + OUTSIDE_COL + " INTEGER, " // boolean
+                + SOCIALIZED_COL + " INTEGER, " // boolean
+                + MEDITATED_COL + " INTEGER, " // boolean
+                + READ_COL + " INTEGER)"; // boolean
 
         // TODO modify tracker columns - sreya
-        String trackerQuery = "CREATE TABLE Tracker ("
-                + "id  INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + "date TEXT, "
-                + "exercised INTEGER, " // boolean
-                + "outside INTEGER, " // boolean
-                + "socialized INTEGER, " // boolean
-                + "meditated INTEGER, " // boolean
-                + "read INTEGER)"; // boolean
+        String trackerQuery = "CREATE TABLE" + TABLE_THREE_NAME + "("
+                + UNAME_COL + " TEXT PRIMARY KEY, "
+                + EXERCISED_COL + " INTEGER, " // boolean
+                + OUTSIDE_COL + " INTEGER, " // boolean
+                + SOCIALIZED_COL + " INTEGER, " // boolean
+                + MEDITATED_COL + " INTEGER, " // boolean
+                + READ_COL + " INTEGER)"; // boolean
 
         // running the queries
 //        db.execSQL(loginQuery);
@@ -73,30 +104,30 @@ public class DBClass extends SQLiteOpenHelper {
 //        db.insert(TABLE_NAME, null, values);
 //        db.close();
 //    }
-//    public String selectQuery(String fieldname){
-//        String fieldvalue;
-//        String query="SELECT "+fieldname+" FROM "+TABLE_NAME;
-//        SQLiteDatabase db = this.getReadableDatabase();
-//        Cursor cur=db.rawQuery(query,null); //Runs the provided SQL and returns a Cursor over the result set.
-//
-//
-//        //  extract.moveToLast();
-//        cur.moveToFirst();
-//        String val="";
-//        while (cur.moveToNext()) {  //contains all rows. traversing
-//            // if (cur.getInt(1)) {
-//            String title = cur.getString(0);
-//            Log.d("Check cursor","====="+title);
-//            if(cur.getPosition()==cur.getCount()-1){  //getting last row.
-//                val=cur.getString(0);
-//            }
-//        }
-//        cur.close();
-//
-//        db.close();
-//        return val;
-//
-//    }
+      public String selectQuery(String fieldname){
+          String fieldvalue;
+          String query="SELECT "+fieldname+" FROM "+TABLE_NAME;
+          SQLiteDatabase db = this.getReadableDatabase();
+          Cursor cur=db.rawQuery(query,null); //Runs the provided SQL and returns a Cursor over the result set.
+
+
+          //  extract.moveToLast();
+          cur.moveToFirst();
+          String val="";
+          while (cur.moveToNext()) {  //contains all rows. traversing
+              // if (cur.getInt(1)) {
+              String title = cur.getString(0);
+              Log.d("Check cursor","====="+title);
+              if(cur.getPosition()==cur.getCount()-1){  //getting last row.
+                  val=cur.getString(0);
+              }
+          }
+          cur.close();
+
+          db.close();
+          return val;
+
+      }
 //
 //    public String selectConditionQuery(String fieldname,String condition){
 //        String fieldvalue;
@@ -125,9 +156,9 @@ public class DBClass extends SQLiteOpenHelper {
 //
 //    }
 //
-//    public void updateTable(String field1,String fieldvalue1,String field2,String fieldvalue2){
-//        SQLiteDatabase db = this.getReadableDatabase();
-//        db.execSQL("UPDATE "+TABLE_NAME+" SET"+" "+field1 +"="+"'"+fieldvalue1+"',"+field2+"="+"'"+fieldvalue2+"'"); //special character.
-//        db.close();
-//    }
+      public void updateTable(String field1,String fieldvalue1,String field2,String fieldvalue2){
+          SQLiteDatabase db = this.getReadableDatabase();
+          db.execSQL("UPDATE "+TABLE_NAME+" SET"+" "+field1 +"="+"'"+fieldvalue1+"',"+field2+"="+"'"+fieldvalue2+"'"); //special character.
+          db.close();
+      }
 }
